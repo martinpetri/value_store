ad38747e640a616c9a7110ee7f5254db112f2429
martinpetri <martin.petri@posteo.de>
2022-02-07 18:55:41 +0100

    Error display size

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -53,7 +53,7 @@ document.addEventListener('turbolinks:load', () => {
     var topic_values = JSON.parse(json_data).values
 
     var display = document.getElementById(topic_name + '_display');
-    display.innerHTML = topic_values[topic_values.length - 1];
+    display.innerHTML = '<h1 class="display-2">' + topic_values[topic_values.length - 1] + '</h2>';
   
     
     for (i = 1; i < topics.length; i++){
